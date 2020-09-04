12fe85eb163a0d89ff6729975967a814cb0ff9bc
wa1id <wa1id@users.noreply.github.com>
2020-09-05 00:44:39 +0200

    Add Home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
-    
+
     //BEM
     <div className="app">
       <h1>Lets build airbnb</h1>
+    <Home />
+    
     </div>
   );
 }
