b037354ef794f5c27be6c96f4976d92c177e03c6
wa1id <wa1id@users.noreply.github.com>
2020-09-05 02:06:57 +0200

    banner info styling

--- src/Banner.js
+++ src/Banner.js
@@ -5,7 +5,6 @@ import Button from '@material-ui/core/Button';
 function Banner() {
     return (
         <div className='banner'>
-            <h1>banner</h1>
             <div className='banner__info'>
                 <h1>Get out and stretch your imagination</h1>
                 <h5>Plan a different kind of getaway to uncover the hidden gems near you.</h5>
