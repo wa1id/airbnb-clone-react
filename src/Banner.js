3e25e2ac0e32801ff4bf7edd272c0dee7766bfd7
wa1id <wa1id@users.noreply.github.com>
2020-09-05 02:02:54 +0200

    add text and button on banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Banner.css'
+import Button from '@material-ui/core/Button';
 
 function Banner() {
     return (
         <div className='banner'>
             <h1>banner</h1>
+            <div className='banner__info'>
+                <h1>Get out and stretch your imagination</h1>
+                <h5>Plan a different kind of getaway to uncover the hidden gems near you.</h5>
+                <Button variant='outlined'>Explore nearby</Button>
+            </div>
         </div>
     )
 }
