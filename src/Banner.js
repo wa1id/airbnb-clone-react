7e92708b255ca4541a7ee0aa9330d3335468c13e
wa1id <wa1id@users.noreply.github.com>
2020-09-05 02:29:16 +0200

    clicking on button shows and hides h1

--- src/Banner.js
+++ src/Banner.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState} from 'react'
 import './Banner.css'
 import Button from '@material-ui/core/Button';
 
 function Banner() {
+    const [showSearch, setShowSearch] = useState (false);
+
     return (
         <div className='banner'>
             <div className='banner__search'>
-                <Button className='banner__searchButton' variant='outlined'>Search Dates</Button>
+                {showSearch && <h1>SHOW DATE PICKER</h1>}
+                <Button onClick={() => setShowSearch(!showSearch)} className='banner__searchButton' variant='outlined'>Search Dates</Button>
             </div>
             <div className='banner__info'>
                 <h1>Get out and stretch your imagination</h1>
