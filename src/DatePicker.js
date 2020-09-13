0c081b1f2a84ed81f7b47aaafeae01d1181a71b4
wa1id <wa1id@users.noreply.github.com>
2020-09-13 13:06:39 +0200

    datepicker css

--- src/DatePicker.js
+++ src/DatePicker.js
@@ -3,13 +3,15 @@ import './DatePicker.css'
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { DateRangePicker } from 'react-date-range'
+import PeopleIcon from '@material-ui/icons/People';
+import Button from '@material-ui/core/Button';
 
 function DatePicker() {
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
 
     const selectionRange = {
-        startDate: startDate, 
+        startDate: startDate,
         endDate: endDate,
         key: 'selection'
     };
@@ -21,7 +23,14 @@ function DatePicker() {
 
     return (
         <div className='datepicker'>
-            <DateRangePicker ranges={[selectionRange]} onChange= {handleSelect} />
+            <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
+            <h2>
+                Number of guests <PeopleIcon />
+            </h2>
+            <input min={0}
+                defaultValue={2}
+                type='number' />
+            <Button>Search Airbnb</Button>
         </div>
     )
 }
